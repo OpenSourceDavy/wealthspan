aba786d5e672a03595d13685566938e98d7d8b3e
daicheng <daicheng@bytedance.com>
2026-03-19 17:39:17 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,22 @@
 import type { NextConfig } from "next";
 import path from "path";
 
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
+
+// const nextConfig: NextConfig = {
+//   output: "export",
+//   basePath: isProd ? "/wealthspan" : "",
+//   assetPrefix: isProd ? "/wealthspan/" : "",
+//   turbopack: {
+//     root: path.resolve(__dirname),
+//   },
+// };
+
+// export default nextConfig;
+
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/wealthspan" : "",
-  assetPrefix: isProd ? "/wealthspan/" : "",
   turbopack: {
     root: path.resolve(__dirname),
   },
